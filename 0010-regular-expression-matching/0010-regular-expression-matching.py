a81c6e4257719e38c162e0ad3db1fea06de6cc45
RAVANAM NAGA VINOD NAIDU <vinodnaidu4334@gmail.com>
2026-07-17 20:30:28 +0530

    Time: 10 ms (68.1%), Space: 12.8 MB (15.11%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isMatch(self, s, p):
+
+        memo = {}
+
+        def dp(i, j):
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if j == len(p):
+                return i == len(s)
+
+            firstMatch = i < len(s) and (s[i] == p[j] or p[j] == '.')
+
+            if j + 1 < len(p) and p[j + 1] == '*':
+                ans = dp(i, j + 2) or (firstMatch and dp(i + 1, j))
+            else:
+                ans = firstMatch and dp(i + 1, j + 1)
+
+            memo[(i, j)] = ans
+            return ans
+
+        return dp(0, 0)
