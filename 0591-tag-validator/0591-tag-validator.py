c7a2d1a8e415bc27909bcd3c3b4e876fca972df0
RAVANAM NAGA VINOD NAIDU <vinodnaidu4334@gmail.com>
2026-07-20 18:22:59 +0530

    Time: 0 ms (100%), Space: 12.5 MB (0%) - LeetHub

---
+++ 0591-tag-validator/0591-tag-validator.py
@@ -0,0 +1,58 @@
+class Solution:
+    def isValid(self, code):
+
+        stack = []
+        i = 0
+        n = len(code)
+
+        while i < n:
+
+            if i > 0 and not stack:
+                return False
+
+            if code.startswith("<![CDATA[", i):
+
+                if not stack:
+                    return False
+
+                j = code.find("]]>", i)
+                if j == -1:
+                    return False
+
+                i = j + 3
+
+            elif code.startswith("</", i):
+
+                j = code.find(">", i)
+                if j == -1:
+                    return False
+
+                tag = code[i + 2:j]
+
+                if not stack or stack[-1] != tag:
+                    return False
+
+                stack.pop()
+                i = j + 1
+
+            elif code.startswith("<", i):
+
+                j = code.find(">", i)
+                if j == -1:
+                    return False
+
+                tag = code[i + 1:j]
+
+                if len(tag) < 1 or len(tag) > 9:
+                    return False
+
+                if not tag.isalpha() or not tag.isupper():
+                    return False
+
+                stack.append(tag)
+                i = j + 1
+
+            else:
+                i += 1
+
+        return len(stack) == 0
