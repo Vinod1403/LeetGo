ce69e0569cc5d007baef5ef249327fb7eb62f787
RAVANAM NAGA VINOD NAIDU <vinodnaidu4334@gmail.com>
2026-07-25 11:33:44 +0530

    Time: 0 ms (100%), Space: 13.9 MB (63.54%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfBeams(self, bank):
+
+        prev = 0
+        beams = 0
+
+        for row in bank:
+
+            devices = row.count('1')
+
+            if devices == 0:
+                continue
+
+            beams += prev * devices
+            prev = devices
+
+        return beams
